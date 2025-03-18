d0d11af3ec08b27d682a7b933e814969040a8acc
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-18 17:13:36 -0500

    figuring things out

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './assets/css/style.css';
+import GradientBox from './components/GradientBox';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <div id="top-banner"></div>
+      <GradientBox />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer id="extra-footer">
+      <p>&copy; 2025 Alex Menendez</p>
+      <a href="#">GitHub</a> | 
+      <a href="#">LinkedIn</a>
+    </footer>
+  );
+}
+
+export default Footer;
