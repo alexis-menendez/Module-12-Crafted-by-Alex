abd5b42f9a20cc13557f414cb57ce1a3107803ba
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-20 20:11:45 -0500

    imported fonts

--- portfolio/src/components/Navigation.jsx
+++ portfolio/src/components/Navigation.jsx
@@ -4,16 +4,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "../assets/css/Navigation.module.css";
 
+
 const Navigation = () => {
   return (
     <>
-      <div className={styles.navigationContent}></div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Me</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
+      <nav className={styles.navbar}>
+        <div className={styles.leftNav}>
+          <Link to="/" className={styles.homeLink}>Alex Menendez</Link>
+        </div>
+        <ul className={styles.rightNav}>
+          <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
           <li><Link to="/resume">Resume</Link></li>
         </ul>
       </nav>
